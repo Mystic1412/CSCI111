178e3e9f19df61160c58028788e9b0fd423b0987
Mystic <nicholelin3929@gmail.com>
2023-09-19 10:55:36 -0400

    Learning more abt for loops

--- Lecture7.cpp
+++ Lecture7.cpp
@@ -21,10 +21,31 @@ for (x; x>0; x=x/10){
 }
 cout<<endl;
 
+//prints the squared till 20
 for (x=1; x <=20; x++){
     cout<<x<<" x "<<x<<" = "<<x*x<<endl;
 }
 
+//prints odd #s
+for (x=1; x<=20; x++){
+    if (x%2 != 0){
+        cout<<x<<" ";
+    }
+}
 
+cout<<endl;
+
+for (x=1; x<=15; x++){
+    cout<<".";
+}
+
+
+for (x=1; x<=20; x++){
+    //a row of 5 "*"
+    for (int i; i<=x; i++){
+        //print 1 *s
+        cout<<"*";
+    }
+} 
 return 0;
 }
