66dd12027436a59795c096b74ace540016d34534
Mystic <nicholelin3929@gmail.com>
2023-09-19 10:32:25 -0400

    more leacture notes abt for loops

--- Lecture7.cpp
+++ Lecture7.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 using namespace std;
+//For loops
 
 int main(){
-    
+/*
+for(set up counter; testing counter; change counter){
+    Instruction(s) to repeat;
+}
+ 
+ int x=10;
+ while(x>0){
+     cout<<x<<" Hello\n";
+     x--;}
+*/ 
+int x;
+cout<<"Enter a number: ";
+cin>>x;
+for (x; x>0; x=x/10){
+    cout<<x%10<<endl;
+}
+cout<<endl;
+
+for (x=1; x <=20; x++){
+    cout<<x<<" x "<<x<<" = "<<x*x<<endl;
+}
+
+
+return 0;
 }
