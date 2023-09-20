c0c45708340ee95df65ffb089b6aa94da155134f
Mystic <nicholelin3929@gmail.com>
2023-09-20 09:36:01 -0400

    Finished exersice 2

--- Lab/Lab7.cpp
+++ Lab/Lab7.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main() {
-    int num;
+    int num, counter=1;
     cout<<"Enter a postive number: "<<endl;
     cin>>num;
     while (num<0){
         cout<<"enter a postive number: "<<endl;
         cin>>num;
+    }
+
+    while (counter<=20){
+        cout<<counter<<" x "<<num<<" = "<<num*counter<<endl;
+        counter++;
+
     }
 
     return 0;
