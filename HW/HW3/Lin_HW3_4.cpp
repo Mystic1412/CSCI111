3e602df7c123d1f98d22aff93567c99d58358590
Mystic <nicholelin3929@gmail.com>
2023-09-20 22:33:19 -0400

    prime number finder

--- HW/HW3/Lin_HW3_4.cpp
+++ HW/HW3/Lin_HW3_4.cpp
@@ -2,12 +2,26 @@
 using namespace std;
 
 int main(){
-    int x, counter;
+    int x, counter=2;
+    bool prime=true;
     cout<<"Enter a number: ";
     cin>> x;
-    counter =x;
-
-    while(counter>0){
-        x/counter;
+    
+    if (x==0 || x==1){
+        prime=false;
     }
+    if (x%2==0){prime = false;}
+    else{
+        while(counter<=x){
+            if (counter<= x/counter){
+            prime = false;}
+    }}
+
+
+    if (prime ==true){
+        cout<<"The number is prime";
+    }else{
+        cout<<"The number is not prime";
+    }
+    return 0;
 }
