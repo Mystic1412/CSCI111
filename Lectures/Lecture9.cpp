caca38a4cd994ae4afcea63bf6755bae660d2fd5
Mystic <nicholelin3929@gmail.com>
2023-09-26 12:16:56 -0400

    more making triangle with for loops

--- Lectures/Lecture9.cpp
+++ Lectures/Lecture9.cpp
@@ -9,4 +9,25 @@ int main(){
         
         cout<<endl;
     }
+    //upside down triangle
+    for (int r=1; r<=5; r++){ //the row
+        for(int c=9;c>0;c--){ //the colums
+            if(c+r>=6 && c<=r+4){
+               cout<<" "; 
+            }
+            else{cout<<"*";}
+        }
+        cout<<endl;
+    }
+
+    //right side up triangle
+    for (int r=1; r<=5; r++){ //the row
+        for(int c=1;c<=9;c++){ //the colums
+            if(c+r>=6 && c<=r+4){
+               cout<<"*"; 
+            }
+            else{cout<<" ";}
+        }
+        cout<<endl;
+    }
 }
