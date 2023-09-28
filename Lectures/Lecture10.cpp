a4cd4817601e38db2453441f97565516f4eb2867
Mystic <nicholelin3929@gmail.com>
2023-09-28 10:24:48 -0400

    made lecture 10

---
+++ Lectures/Lecture10.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath> //#include <-- is to get a library <cmath> <-- this is a library
+using namespace std;
+
+
+/*What is a function in C++
+- A function is a small piece of code to carry out a single useful task that you can call/use whenever the task is needed
+- Function allows us to code once and use it many times*/
+
+int main(){
+    int x;
+    cout<<"num:";
+    cin>>x;
+    cout<<sqrt(x);
+    cout<<sqrt(9);
+    return 0;
+}
