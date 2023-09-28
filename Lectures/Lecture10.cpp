f12bbcb0cb91ea9fdeac97209b086cd21f6c3f8e
Mystic <nicholelin3929@gmail.com>
2023-09-28 10:54:57 -0400

    added time lib convert time in sec 2 yr since 1970

--- Lectures/Lecture10.cpp
+++ Lectures/Lecture10.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath> //#include <-- is to get a library <cmath> <-- this is a library
-#include <cstdlib>
+#include <cstdlib> //give random
+#include <ctime> //time library
 using namespace std;
 
 
@@ -9,14 +10,16 @@ using namespace std;
 - Function allows us to code once and use it many times*/
 
 int main(){
-    int x;
-    cout<<"num:";
-    cin>>x;
+    int t = time(0); //time is in seconds
+    srand(time(0)); //srand lets you put a number for the random function //time(0) 0 means null
     cout<<sqrt(9); //sqrt of 9 function
-    cout<<endl;
+    cout<<endl<<endl;
+    cout<<"time = "<<t; 
+    cout<<"time = "<<t/60.0/60/24/365.25<<" years since 1970";
+    cout<<endl<<endl;
 
     for (int i=1; i<=10; i++){
-        cout<<rand()%100<<endl; //rand is random
+        cout<<rand()%10<<endl; //rand is random
     }
     return 0;
 }
