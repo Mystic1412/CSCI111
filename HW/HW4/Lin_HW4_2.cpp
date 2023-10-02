8dda7ff4b3a2abd2da47978539d7184e13ed1ff6
Mystic <nicholelin3929@gmail.com>
2023-10-01 22:15:52 -0400

    finished hw4.2

---
+++ HW/HW4/Lin_HW4_2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y;
+    cout<<"Enter two positive intergers: ";
+    cin>> x>>y;
+    if (x>y){
+        for (int counter=0;counter<y;counter++){
+            cout<<"X";
+        }
+    }
+    else if (x<y)
+    {
+        for (int counter=0;counter<x;counter++){
+            cout<<"X";
+        }
+    }
+    else{
+        for (int counter=0;counter<x;counter++){
+            cout<<"X";
+        }
+    }
+}
