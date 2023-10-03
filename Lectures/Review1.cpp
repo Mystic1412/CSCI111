c0171ff2723820d67e3fd855e09049709fdce532
Mystic <nicholelin3929@gmail.com>
2023-10-03 11:31:15 -0400

    did problem 4

---
+++ Lectures/Review1.cpp
@@ -0,0 +1,16 @@
+//1 output, 2q for making a complete cpp program, 1 patten
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int x = 6, y = 0, z= 3;
+    double a = 5.0, b= 4.0;
+    cout << x << x * a << endl;
+    cout << (y % z) << "\nn\n" << z / x << endl;
+    if ((a < x) && (b > y)) cout << "Hello" << endl;
+    for ( ; x <= 9; x++) cout << x;
+    cout << endl ;
+    cout << sqrt (b) * sqrt (b) / x * z << endl;
+
+}
