a6596add69a2818a1371d84dcb2168d5700107f7
Mystic <nicholelin3929@gmail.com>
2023-10-03 11:33:39 -0400

    did problem 3

--- Lectures/Review1.cpp
+++ Lectures/Review1.cpp
@@ -4,13 +4,5 @@
 using namespace std;
 
 int main(){
-    int x = 6, y = 0, z= 3;
-    double a = 5.0, b= 4.0;
-    cout << x << x * a << endl;
-    cout << (y % z) << "\nn\n" << z / x << endl;
-    if ((a < x) && (b > y)) cout << "Hello" << endl;
-    for ( ; x <= 9; x++) cout << x;
-    cout << endl ;
-    cout << sqrt (b) * sqrt (b) / x * z << endl;
-
+    
 }
