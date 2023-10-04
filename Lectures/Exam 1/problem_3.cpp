ac012a958e905f53aee2a1ac59e39765b973a197
Mystic <nicholelin3929@gmail.com>
2023-10-03 23:21:55 -0400

    more review

--- Lectures/Exam 1/problem_3.cpp
+++ Lectures/Exam 1/problem_3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int x = 6, y = 0, z= 3;
-    double a = 5.0, b= 4.0;
+    double a = 5, b= 4.0;
     cout << x << x * a << endl;
     cout << (y % z) << "\nn\n" << z / x << endl;
     if ((a < x) && (b > y)) cout << "Hello" << endl;
--- Lectures/Review1.cpp
+++ Lectures/Review1.cpp
@@ -4,5 +4,28 @@
 using namespace std;
 
 int main(){
-    
+    // 8 9 RM 017 4 question: 1 output question, 2 complete programs; 1 pattern 1 not,  
+    /*
+       X
+      XX
+     XXX
+    XXXX
+    */
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    for (int r=1; r<=2*n-1; r++){
+        for(int c=1; c<=n; c++){
+            if (r+c>=n+1&&r-c<=n-1){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
 }
