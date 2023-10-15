70da0618bf2dfdbae0516aa92af359d77b73ac56
Mystic <nicholelin3929@gmail.com>
2023-10-15 17:02:30 -0400

    finished hw5.2

--- HW/HW5/Lin_HW5_2.cpp
+++ HW/HW5/Lin_HW5_2.cpp
@@ -7,15 +7,11 @@ int main(){
     int num1, num2;
     cout<<"Enter two positive intergers with a difference of at least 10: ";
     cin>>num1>>num2;
-    while((num1-num2 && num2-num1)<=10){
+    while(num1-num2 <=10){
         cout<<"Invalid input! Try again: ";
         cin>>num1>>num2;
     }
 
-    if(num1>num2){
-        cout<<"Random number between "<<num1<< " and "<< num2<< " is "<<(num1-rand()%(num1-num2+1));
-    }else{
-        cout<<"Random number between "<<num1<< " and "<< num2<< " is "<<(num2-rand()%(num2-num1+1));
-    }
+    cout<<"Random number between "<<num1<< " and "<< num2<< " is "<<(num1-rand()%(num1-num2+1));
     return 0;
 }
