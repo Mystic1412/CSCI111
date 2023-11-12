4e7811c0186f32a4e0a91ea956c8bdb1936c7918
Mystic <nicholelin3929@gmail.com>
2023-11-12 16:06:29 -0500

    finished hw 7

--- HW/HW7/Lin_HW7_2.cpp
+++ HW/HW7/Lin_HW7_2.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
-int catEars(int cats){
-    
-}
-int main(){
 
+int catEars(int cats){
+    if (cats<=1) return 2;
+    return catEars(cats-1)+2;
+}
+
+int main(){
+    cout<<catEars(6)<<" cat ears";
     return 0;
 }
--- HW/HW7/Lin_HW7_3.cpp
+++ HW/HW7/Lin_HW7_3.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int num_of_digits(int n){
+    if (n< 10) return 1;
+    return num_of_digits(n/10)+1;
+}
 
+int main(){
+    cout<<num_of_digits(167324);
     return 0;
 }
