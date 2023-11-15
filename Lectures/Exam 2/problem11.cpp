aed31fb4c7a67ac8fb4f4e392c81ff288936477e
Mystic <nicholelin3929@gmail.com>
2023-11-14 23:09:34 -0500

    finished problem 11

---
+++ Lectures/Exam 2/problem11.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int adjust(int a[],int x){
+    for (int i=0;i<x; i++){
+        a[i] = a[i]+((-1)+rand()%(2-(-2)+1));
+    }
+    return 0;
+}
+int main(){
+    srand(time(0));
+    int x[5]={3,1,4,1,5};
+    adjust(x,5);
+    cout<<x[1]<<endl;
+    return 0;
+}
