272daf7e0997cf62f296d5b723d822a0609e614d
Mystic <nicholelin3929@gmail.com>
2023-11-27 13:11:58 -0500

    hw9

---
+++ HW/HW9/Lin_HW9_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void countPrint(int array[], int length){
+    //need a counter array (another array)
+    
+    for (int i=0; i<length;i++){
+        array[i];
+    }
+
+}
+int main(){
+    int test[] = {0,1,7,3,7,2,1,1,7,8,1};
+    return 0;
+}
---
+++ HW/HW9/Lin_HW9_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//funciton,
+bool search2d(int array[][3],int r, int c, int num);
+
+int main(){
+
+    return 0;
+}
