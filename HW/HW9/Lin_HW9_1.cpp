5cef13b522fa9e6e7e6f39e58e99e9be2fe0deff
Mystic <nicholelin3929@gmail.com>
2023-11-28 13:48:35 -0500

    finished hw 9.1

--- HW/HW9/Lin_HW9_1.cpp
+++ HW/HW9/Lin_HW9_1.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 void countPrint(int array[], int length){
-    //need a counter array (another array)
-    
-    for (int i=0; i<length;i++){
-        array[i];
-    }
+    for(int num=0; num<length;num++){
+        int count=0;
+        for (int i=0; i<length;i++){
+            if(num== array[i]){
+                count++;
+            }
+        }
 
-}
+        if(count!=0){
+            cout<<num<<" has a count of "<< count<<endl;
+        }
+    }
 int main(){
-    int test[] = {0,1,7,3,7,2,1,1,7,8,1};
+    int test[] = {0,1,7,3,7,2,1,1,0,2};
+    countPrint(test, 10);
     return 0;
 }
