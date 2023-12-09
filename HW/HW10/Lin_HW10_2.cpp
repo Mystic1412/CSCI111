4aaacb828d906ce9586f331bb11d2f98f2da3df0
Mystic <nicholelin3929@gmail.com>
2023-12-08 23:57:21 -0500

    Finished hw 10.2

--- HW/HW10/Lin_HW10_2.cpp
+++ HW/HW10/Lin_HW10_2.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 using namespace std;
+string lowerCaseString(string &x){
+     for(int i =0; i<x.size(); i++){
+        if (x[i] >= 65 && x[i]<=90) {
+            x[i] = x[i]+32;
+        }
+     }
+     return x;
+}
 
+string upperCaseString(string &x){
+    for(int i =0; i<x.size(); i++){
+        if (x[i] >= 97 && x[i]<=122) {
+            x[i] = x[i]-32;
+        }
+     }
+     return x;
+}
+
+string undulatingString(string &x){
+    for(int i =0; i<x.size(); i++){
+        if(i%2 == 0){
+            if (x[i] >= 65 && x[i]<=90) {
+            x[i] = x[i]+32;
+        }
+        }
+    }
+    return x;
+}
 int main(){
+    string s;
+    cout<<"Type something: ";
+    cin>>s;
+
+    lowerCaseString(s);
+    cout<<"Fully lower case:\n";
+    cout<<s<<endl;
+
+    upperCaseString(s);
+    cout<<"Fully upper case:\n";
+    cout<<s<<endl;
+
+    undulatingString(s);
+    cout<<"Undulating case:\n";
+    cout<<s<<endl;
     return 0;
 }
--- test.cpp
+++ test.cpp
@@ -4,8 +4,9 @@ using namespace std;
 
 int main(){
     string x = "bob";
-    cout<<x.size();
-    cout<<x[0];
+    cout<<x.size()<<endl;
+    cout<< x[0]<<endl;
+    cout<<(char)x[0]+2<<endl;
     return 0;
 }
 
