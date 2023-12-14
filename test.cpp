cb432bbeddab116ac892c4cef7e4409bdb4ca5c6
Mystic <nicholelin3929@gmail.com>
2023-12-13 22:32:58 -0500

    testing

--- test.cpp
+++ test.cpp
@@ -3,10 +3,15 @@
 using namespace std;
 
 int main(){
-    string x = "bob";
-    cout<<x.size()<<endl;
-    cout<< x[0]<<endl;
-    cout<<(char)x[0]+2<<endl;
+    int n =4;
+    for (int r=n-1;r>=0;r--){
+        for(int c= -(n-1); c<n; c++){
+            if(c<-r||c>r||(c+r)%2!=0)cout<<" ";
+            else cout<<"X";
+        }
+    cout<<endl;
+    }
+    cout<<0%2;
     return 0;
 }
 
